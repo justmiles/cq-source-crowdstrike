fae07e57263a113e22a21e6cb7d7c9e3591836dd
Razin Idzuddin <razin99@gmail.com>
2023-12-20 13:59:22 +1100

    batch get ZTA, instead of trying one Id at a time

--- resources/services/zta.go
+++ resources/services/zta.go
@@ -2,16 +2,15 @@ package services
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/justmiles/cq-source-crowdstrike/client"
+	"encoding/json"
+	"time"
 
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
 	"github.com/crowdstrike/gofalcon/falcon"
-	"github.com/crowdstrike/gofalcon/falcon/client/hosts"
 	"github.com/crowdstrike/gofalcon/falcon/client/zero_trust_assessment"
 	"github.com/crowdstrike/gofalcon/falcon/models"
+	"github.com/justmiles/cq-source-crowdstrike/client"
 )
 
 func ZTAs() *schema.Table {
@@ -23,52 +22,81 @@ func ZTAs() *schema.Table {
 }
 
 func fetchZTAs(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+
+	hostsChan := make(chan any)
+	defer close(hostsChan)
+
+	errChan := make(chan error)
+	defer close(errChan)
+	go func() {
+		errChan <- fetchHosts(ctx, meta, parent, hostsChan)
+	}()
+
 	c := meta.(*client.Client)
-
-	var offset int64 = 0
-	var limit int64 = 100
-	for {
-
-		queryRespOK, err := c.CrowdStrike.Hosts.QueryDevicesByFilter(&hosts.QueryDevicesByFilterParams{
-			Context: ctx,
-			Limit:   &limit,
-			Offset:  &offset,
-		})
+	for batch := range batchedHosts(hostsChan, 100, 5*time.Second) {
+		result, err := resolveZTAs(ctx, c, batch)
 		if err != nil {
-			return fmt.Errorf("could not query hosts: %s", err.Error())
+			return err
 		}
-
-		queryResponse := queryRespOK.GetPayload()
-		if err = falcon.AssertNoError(queryResponse.Errors); err != nil {
-			return fmt.Errorf("Error querying hosts: %s", falcon.ErrorExplain(err))
-		}
-
-		for _, hostId := range queryResponse.Resources {
-			params := zero_trust_assessment.NewGetAssessmentV1ParamsWithContext(ctx).WithIds([]string{hostId})
-			ztaOK, err := c.CrowdStrike.ZeroTrustAssessment.GetAssessmentV1(params)
-			if err != nil {
-				// If one of the host id is not enrolled to zta, whole thing fails
-				// By checking one host id at a time, we are able to get ALL zta assesments
-				fmt.Printf("could not get ZTA: %s\n", err.Error())
-				continue
-			}
-
-			ztaResponse := ztaOK.GetPayload()
-			if err = falcon.AssertNoError(ztaResponse.Errors); err != nil {
-				return fmt.Errorf("Error querying ZTA: %s", falcon.ErrorExplain(err))
-			}
-
-			for _, zta := range ztaResponse.Resources {
-				res <- zta
-			}
-		}
-
-		offset += limit
-
-		if *queryResponse.Meta.Pagination.Total == int64(*queryResponse.Meta.Pagination.Offset) {
-			break
+		for _, zta := range result.Resources {
+			res <- zta
 		}
 	}
 
-	return nil
+	return <-errChan
+}
+
+func batchedHosts(hosts <-chan any, maxBatchSize int, maxTimeout time.Duration) chan []string {
+	batches := make(chan []string)
+
+	go func() {
+		defer close(batches)
+
+		for keepGoing := true; keepGoing; {
+			var batch []string
+			expire := time.After(maxTimeout)
+			for {
+				select {
+				case host, ok := <-hosts:
+					if !ok {
+						keepGoing = false
+						goto done
+					}
+					batch = append(batch, *host.(*models.DeviceapiDeviceSwagger).DeviceID)
+					if len(batch) == maxBatchSize {
+						goto done
+					}
+				case <-expire:
+					return
+				}
+			}
+		done:
+			if len(batch) > 0 {
+				batches <- batch
+			}
+		}
+	}()
+
+	return batches
+}
+
+func resolveZTAs(ctx context.Context, c *client.Client, hostIds []string) (*models.DomainAssessmentsResponse, error) {
+	ztaOK, err := c.CrowdStrike.ZeroTrustAssessment.GetAssessmentV1(&zero_trust_assessment.GetAssessmentV1Params{
+		Ids:     hostIds,
+		Context: ctx,
+	})
+	if err != nil {
+		// TODO: differentiate between 404 and other kinds of error
+		c.Logger.Warn().Err(err).Msg("cannot get zta")
+		rawData, err := falcon.ErrorExtractPayload(err).MarshalBinary()
+		if err != nil {
+			return nil, err
+		}
+		foundResults := &models.DomainAssessmentsResponse{}
+		if err := json.Unmarshal(rawData, foundResults); err != nil {
+			return nil, err
+		}
+		return foundResults, nil
+	}
+	return ztaOK.GetPayload(), nil
 }
